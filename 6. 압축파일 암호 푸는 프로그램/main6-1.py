98c1b24ed7e6593b12f620cb29070e01b618fbbd
yeji <yeji.lee.kim@gmail.com>
2023-02-05 22:35:50 +0900

    delete

--- 6. 압축파일 암호 푸는 프로그램/main6-1.py
+++
@@ -1,9 +0,0 @@
-import itertools
-
-password_string = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-for len in range(1,4):
-    to_attempt = itertools.product(password_string, repeat=len)
-    for attempt in to_attempt:
-        passwd = ''.join(attempt)
-        print(passwd)
